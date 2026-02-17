5589f802d99f95d1ad14ba1459f93e0a70eb0377
asraful islam <asraful42422@gmail.com>
2026-02-17 21:46:56 +0600

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        primary: "#16a34a",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
