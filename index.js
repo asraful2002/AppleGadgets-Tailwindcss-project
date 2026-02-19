4e4c9173e00571fad15d1b9920afce13348c8b2b
asraful islam <asraful42422@gmail.com>
2026-02-19 14:36:01 +0600

    change

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("hello");
+// let num=20;
+// if (num%2 ===0) {
+//     console.log("even")
+// } else {
+//     console.log('odd');
+// }
+
+let age = 18;
+if (age < 13) {
+  console.log("chaild");
+} else if (age <= 19) {
+  console.log("teen");
+} else {
+  console.log("adult");
+}
+
+let day = 1;
+
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("Invalid");
+}
